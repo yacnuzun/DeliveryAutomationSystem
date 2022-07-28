b4d903513fe4d2eda5d855a08632e9531e00139a
yacnuzun <yacn.uzun@gmail.com>
2022-07-28 22:16:38 +0300

    Singleton Added.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,9 +1,19 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSingleton<IOrderProductDal, EfOrderProductDal>();
+builder.Services.AddSingleton<IOrderDal, EfOrderDal>();
+builder.Services.AddSingleton<IOrderService, OrderManager>();
+builder.Services.AddSingleton<IDeliveryDal, EfDeliveryDal>();
+builder.Services.AddSingleton<IDeliveryService, DeliveryManager>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
