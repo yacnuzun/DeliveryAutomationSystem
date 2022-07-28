0441c9509ce06c7b2d0ebcf869ee8971e7aed6a4
yacnuzun <yacn.uzun@gmail.com>
2022-07-28 21:30:29 +0300

    Add project files.

---
+++ Entity/Concrete/Delivery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Delivery : IEntity
+    {
+        public int DeliveryId { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public string VehicleNumber { get; set; }
+        public string CargoMan { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ Entity/Concrete/Order.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Order : IEntity
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string EndPoint { get; set; }
+        public string StartPoint { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
